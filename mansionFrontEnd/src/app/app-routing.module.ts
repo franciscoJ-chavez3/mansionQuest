29c4b93646165bfa8a3332fb97b89af61a8c76cc
franciscoJ-chavez3 <fjchavez@outlook.com>
2020-03-30 17:42:10 -0700

    added bootstrap and routing

--- mansionFrontEnd/src/app/app-routing.module.ts
+++ mansionFrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OptionsComponent } from './options/options.component';
+import { GameComponent } from './game/game.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'options', component: OptionsComponent },
+  { path: 'game', component: GameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mansionFrontEnd/src/app/app.module.ts
+++ mansionFrontEnd/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OptionsComponent } from './options/options.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OptionsComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
